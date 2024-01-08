289a45b7f48e7a92d437a7ce70c6d55b6bf1de3b
Zelim <infinityeye@bk.ru>
2024-01-08 03:34:32 +0300

    Update completed the second task

--- 01.js
+++ 01.js
@@ -1,4 +1,12 @@
 //Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
-
 let myFirstName = "Zelim";
 let myLastName = "Digiev";
+
+//Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
