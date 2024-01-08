90997ccfc0729797b772ed32a2103ee64771f5f3
Zelim <infinityeye@bk.ru>
2024-01-08 04:03:46 +0300

    Update completed the third task

--- 01.js
+++ 01.js
@@ -3,10 +3,21 @@ let myFirstName = "Zelim";
 let myLastName = "Digiev";
 
 //Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
-var a = 5;
+var a = 5; ()
 var b = 10;
 var c = "I am a";
 
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+/* Modify the existing declarations and assignments so their names use camelCase.
+Do not create any new variables.
+*/
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
